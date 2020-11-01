bb19c315c4d4f15bebac8417b8f32bbd82d7050a
Bogdan <bogdankuzzmin@gmail.com>
2020-11-01 22:24:50 +0600

    app: - added "toggle button" to show/hide random-planet component - added "error button" to see how "component did catch" works - added error indicator if app gets an error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,27 +2,56 @@ import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import PeoplePage from '../people-page';
+import ErrorIndicator from '../error-indicator';
+import ErrorButton from '../error-button';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  state = {
+    showRandomPlanet: true,
+    hasError: false,
+  }
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
+
+  componentDidCatch() {
+     console.log(`error`);
+     this.setState({hasError: true});
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        {planet}
+
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
         </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
+
+        <PeoplePage />
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
