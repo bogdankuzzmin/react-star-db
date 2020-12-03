c22052111155d1c517af49f1b6d539bb95e07525
Bogdan <bogdankuzzmin@gmail.com>
2020-12-04 00:43:48 +0600

    added two item detail components for refactoring item details

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import PeoplePage from '../people-page';
+// import PeoplePage from '../people-page';
 import ErrorIndicator from '../error-indicator';
 import ErrorButton from '../error-button';
 
 import './app.css';
+import ItemDetails from "../person-details";
+import Row from '../row';
+import ErrorBoundry from "../error-boundry";
+import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component {
+  swapiService = new SwapiService();
+
   state = {
     showRandomPlanet: true,
     hasError: false,
@@ -35,22 +41,42 @@ export default class App extends Component {
       <RandomPlanet/> :
       null;
 
+    const {getPerson, getStarship, getPersonImage, getStarshipImage} = this.swapiService;
+
+    const personDetails = (
+      <ItemDetails
+          itemId={11}
+          getData={getPerson}
+          getImageUrl={getPersonImage} />
+    );
+
+    const starshipDetails = (
+        <ItemDetails
+            itemId={5}
+            getData={getStarship}
+            getImageUrl={getStarshipImage} />
+    );
+
     return (
-      <div className="stardb-app">
-        <Header />
-        {planet}
+      <ErrorBoundry>
+        <div className="stardb-app">
+          <Header />
+          {planet}
 
-        <div className="row mb2 button-row">
-          <button
-            className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet}>
-            Toggle Random Planet
-          </button>
-          <ErrorButton />
+          <div className="row mb2 button-row">
+            <button
+              className="toggle-planet btn btn-warning btn-lg"
+              onClick={this.toggleRandomPlanet}>
+              Toggle Random Planet
+            </button>
+            <ErrorButton />
+          </div>
+
+          <Row left={personDetails} right={starshipDetails} />
+
+          {/*<PeoplePage />*/}
         </div>
-
-        <PeoplePage />
-      </div>
+      </ErrorBoundry>
     );
   }
 }
