c6ff922aff8ee72624318bf7441ed55a38ddd1c0
Bogdan <bogdankuzzmin@gmail.com>
2020-12-07 02:44:55 +0600

    added Records to ItemDetails component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,10 @@ import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-// import PeoplePage from '../people-page';
 import ErrorIndicator from '../error-indicator';
-import ErrorButton from '../error-button';
 
 import './app.css';
-import ItemDetails from "../person-details";
+import ItemDetails, {Record} from "../item-details/item-details";
 import Row from '../row';
 import ErrorBoundry from "../error-boundry";
 import SwapiService from "../../services/swapi-service";
@@ -45,16 +43,26 @@ export default class App extends Component {
 
     const personDetails = (
       <ItemDetails
-          itemId={11}
-          getData={getPerson}
-          getImageUrl={getPersonImage} />
+        itemId={11}
+        getData={getPerson}
+        getImageUrl={getPersonImage}>
+
+        <Record field="birthYear" label="Birth Year" />
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+      </ItemDetails>
     );
 
     const starshipDetails = (
         <ItemDetails
-            itemId={5}
-            getData={getStarship}
-            getImageUrl={getStarshipImage} />
+          itemId={9}
+          getData={getStarship}
+          getImageUrl={getStarshipImage}>
+
+          <Record field="model" label="Model" />
+          <Record field="length" label="Length" />
+          <Record field="passengers" label="Passengers" />
+        </ItemDetails>
     );
 
     return (
@@ -69,7 +77,6 @@ export default class App extends Component {
               onClick={this.toggleRandomPlanet}>
               Toggle Random Planet
             </button>
-            <ErrorButton />
           </div>
 
           <Row left={personDetails} right={starshipDetails} />
