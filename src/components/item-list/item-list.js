751f8fd24d1e11c727ec314f363d2e3bd6656857
Bogdan <bogdankuzzmin@gmail.com>
2020-12-09 23:26:17 +0600

    using "with data" in item list

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,58 +1,35 @@
 
-import React, {Component} from 'react';
+import React from 'react';
 
 import SwapiService from '../../services/swapi-service';
-import Loader from '../loader';
+import {withData} from '../hoc-helpers';
 
 import './item-list.css';
 
-export default class ItemList extends Component {
-  swapiService = new SwapiService();
+const ItemList = (props) => {
+  const {data, itemSelectedHandler, children: renderLabel} = props;
 
-  state = {
-    itemList: null
-  };
-
-  componentDidMount() {
-    const {getData} = this.props;
-
-      getData()
-      .then((itemList) => {
-        this.setState({
-          itemList
-        });
-      })
-  }
-
-  renderItems(arr) {
-    return arr.map((item) => {
-      const {id} = item;
-
-      const label = this.props.children(item);
-
-      return (
-        <li className="list-group-item"
-          key={id}
-          onClick={() => this.props.itemSelectedHandler(id)}>
-          {label}
-        </li>
-      );
-    });
-  }
-
-  render() {
-    const {itemList} = this.state;
-
-    if (!itemList) {
-      return <Loader />
-    }
-
-    const items = this.renderItems(itemList);
+  const items = data.map((item) => {
+    const {id} = item;
+    const label = renderLabel(item);
 
     return (
-      <ul className="item-list list-group">
-        {items}
-      </ul>
+      <li className="list-group-item"
+        key={id}
+        onClick={() => itemSelectedHandler(id)}>
+        {label}
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <ul className="item-list list-group">
+      {items}
+    </ul>
+  )
+};
+
+const {getAllPeople} = new SwapiService();
+
+export default withData(ItemList, getAllPeople);
+
