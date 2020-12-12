c8f0d17cffcf677a837d7fc67770da4835f8da79
Bogdan <bogdankuzzmin@gmail.com>
2020-12-13 04:04:35 +0600

    added hoc for details, map methods for props

--- src/components/sw-components/person-details.js
+++ src/components/sw-components/person-details.js
@@ -1,27 +1,22 @@
-import {SwapiServiceConsumer} from "../swapi-service-context";
 import ItemDetails from "../item-details";
 import {Record} from "../item-details/item-details";
+import {withSwapiService} from '../hoc-helpers';
 
-const PersonDetails = ({itemId}) => {
+const PersonDetails = (props) => {
   return(
-    <SwapiServiceConsumer>
-      {
-        ({getPerson, getPersonImage}) => {
-          return(
-            <ItemDetails
-              itemId={itemId}
-              getData={getPerson}
-              getImageUrl={getPersonImage}>
-
-              <Record field="birthYear" label="Birth Year" />
-              <Record field="gender" label="Gender" />
-              <Record field="eyeColor" label="Eye Color" />
-            </ItemDetails>
-          );
-        }
-      }
-    </SwapiServiceConsumer>
+    <ItemDetails {...props}>
+      <Record field="birthYear" label="Birth Year" />
+      <Record field="gender" label="Gender" />
+      <Record field="eyeColor" label="Eye Color" />
+    </ItemDetails>
   );
 };
 
-export default PersonDetails;
+const mapMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getPerson,
+    getImageUrl: swapiService.getPersonImage
+  };
+};
+
+export default withSwapiService(PersonDetails, mapMethodsToProps);
--- src/components/sw-components/starship-details.js
+++ src/components/sw-components/starship-details.js
@@ -1,28 +1,22 @@
-import {SwapiServiceConsumer} from "../swapi-service-context";
 import ItemDetails from "../item-details";
 import {Record} from "../item-details/item-details";
+import {withSwapiService} from '../hoc-helpers';
 
-const StarshipDetails = ({itemId}) => {
+const StarshipDetails = (props) => {
   return(
-    <SwapiServiceConsumer>
-      {
-        ({getStarship, getStarshipImage}) => {
-          return(
-            <ItemDetails
-              itemId={itemId}
-              getData={getStarship}
-              getImageUrl={getStarshipImage}>
-
-              <Record field="model" label="Model" />
-              <Record field="length" label="Length" />
-              <Record field="passengers" label="Passengers" />
-            </ItemDetails>
-          );
-        }
-      }
-    </SwapiServiceConsumer>
-
+    <ItemDetails {...props}>
+      <Record field="model" label="Model" />
+      <Record field="length" label="Length" />
+      <Record field="passengers" label="Passengers" />
+    </ItemDetails>
   );
 };
 
-export default StarshipDetails;
+const mapMethodsToProps = (swapiService) => {
+  return {
+    getData: swapiService.getStarship,
+    getImageUrl: swapiService.getStarshipImage,
+  };
+};
+
+export default withSwapiService(StarshipDetails, mapMethodsToProps);
